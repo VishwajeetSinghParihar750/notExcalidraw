61c648a85469fdd2c69def380671e9cdd41f810d
Vishwajeet Singh Parihar <svishwajeet750@gmail.com>
2026-02-27 16:37:34 +0000

    arrow done

--- src/classes/Shapes/Arrow.ts
+++ src/classes/Shapes/Arrow.ts
@@ -78,47 +78,87 @@ export class Arrow implements Shape {
         ctx.strokeStyle = this.strokeColor;
         ctx.lineWidth = this.strokeWidth;
 
-        if (this.strokeStyle == "smalldotted") ctx.setLineDash([4, 8]);
-        else if (this.strokeStyle == "dotted") ctx.setLineDash([8, 16]);
+        {
+          ctx.save();
+
+          if (this.strokeStyle == "smalldotted") ctx.setLineDash([4, 8]);
+          else if (this.strokeStyle == "dotted") ctx.setLineDash([8, 16]);
+
+          ctx.beginPath();
+
+          if (this.arrowType == "straight") {
+            ctx.moveTo(this.points[0].x, this.points[0].y);
+            let len = this.points.length;
+
+            for (let i = 1; i < len; i++) {
+              ctx.moveTo(this.points[i - 1].x, this.points[i - 1].y);
+              ctx.lineTo(this.points[i].x, this.points[i].y);
+            }
+          } else if (this.arrowType == "curve") {
+            //
+            ctx.moveTo(this.points[0].x, this.points[0].y);
+
+            let points = [
+              this.points[0],
+              ...this.points,
+              this.points[this.points.length - 1],
+            ];
+            for (let i = 1; i < points.length - 2; i++) {
+              //
+              let p0 = points[i - 1];
+              let p1 = points[i];
+              let p2 = points[i + 1];
+              let p3 = points[i + 2];
+
+              let [b0, b1, b2, b3] = catmullRomToBezier(p0, p1, p2, p3);
+              ctx.bezierCurveTo(b1.x, b1.y, b2.x, b2.y, b3.x, b3.y);
+            }
+          } else if (this.arrowType == "snake") {
+            //
+            let a = this.points[0];
+            ctx.moveTo(a.x, a.y);
+
+            for (let i = 0; i < this.points.length - 2; i++) {
+              let b = this.points[i + 1];
+              let c = this.points[i + 2];
+
+              let check = Math.floor(
+                Math.min(Math.abs(a.x - c.x), Math.abs(a.y - c.y)),
+              );
+
+              ctx.arcTo(b.x, b.y, c.x, c.y, check < 20 ? 0 : 10);
+            }
+            let len = this.points.length;
+            ctx.lineTo(this.points[len - 1].x, this.points[len - 1].y);
+          }
+          ctx.stroke();
+          ctx.restore();
+        }
+        // draw arrow
+
+        let len = this.points.length;
+        let a = this.points[len - 2];
+        let b = this.points[len - 1];
+        //
+        let opposite = b.x < a.x ? -1 : 1;
+
+        let theta = Math.atan((b.y - a.y) / (b.x - a.x));
+        let alpha = theta - Math.PI / 6;
+        let beta = theta + Math.PI / 6;
+
+        let dis = 20 * opposite;
+
+        let dx1 = dis * Math.cos(alpha);
+        let dy1 = dis * Math.sin(alpha);
+        let dx2 = dis * Math.cos(beta);
+        let dy2 = dis * Math.sin(beta);
 
         ctx.beginPath();
-
-        if (this.arrowType == "straight") {
-          ctx.moveTo(this.points[0].x, this.points[0].y);
-          let len = this.points.length;
-
-          for (let i = 1; i < len; i++) {
-            ctx.moveTo(this.points[i - 1].x, this.points[i - 1].y);
-            ctx.lineTo(this.points[i].x, this.points[i].y);
-          }
-          ctx.stroke();
-        } else if (this.arrowType == "curve") {
-          //
-          ctx.moveTo(this.points[0].x, this.points[0].y);
-
-          let points = [
-            this.points[0],
-            ...this.points,
-            this.points[this.points.length - 1],
-          ];
-          for (let i = 1; i < points.length - 2; i++) {
-            //
-            let p0 = points[i - 1];
-            let p1 = points[i];
-            let p2 = points[i + 1];
-            let p3 = points[i + 2];
-
-            let [b0, b1, b2, b3] = catmullRomToBezier(p0, p1, p2, p3);
-            ctx.bezierCurveTo(b1.x, b1.y, b2.x, b2.y, b3.x, b3.y);
-          }
-          ctx.stroke();
-        } else if (this.arrowType == "snake") {
-          //
-          let startPoint = this.points[0];
-          let endPoint = this.points[this.points.length - 1];
-
-          //
-        }
+        ctx.moveTo(b.x, b.y);
+        ctx.lineTo(b.x - dx1, b.y - dy1);
+        ctx.moveTo(b.x, b.y);
+        ctx.lineTo(b.x - dx2, b.y - dy2);
+        ctx.stroke();
       }
       ctx.restore();
     }
--- src/classes/Tools/ArrowTool.ts
+++ src/classes/Tools/ArrowTool.ts
@@ -1,6 +1,7 @@
 import type ShapeManager from "../ShapeManager";
 import { Point } from "../Shapes/Point";
 import { Arrow } from "../Shapes/Arrow";
+import { useToolStyle } from "../../store/Tools.store";
 
 type state = "idle" | "drawingLine" | "drawingPath";
 
@@ -43,7 +44,39 @@ export default class ArrowTool {
       Math.floor(e.clientY),
     );
 
-    if (this.curState == "drawingLine" || this.curState == "drawingPath") {
+    let { arrowType } = useToolStyle.getState();
+
+    if (this.curState == "drawingLine") {
+      if (arrowType == "snake") {
+        //          //
+        let startPoint = this.currentLine!.points[0];
+        let endPoint = curPoint;
+
+        let dx = Math.abs(endPoint.x - startPoint.x);
+        let dy = Math.abs(endPoint.y - startPoint.y);
+
+        if (dx > dy) {
+          let mx = Math.floor((startPoint.x + endPoint.x) / 2);
+          this.currentLine!.points = [
+            startPoint,
+            new Point(mx, startPoint.y),
+            new Point(mx, endPoint.y),
+            endPoint,
+          ];
+        } else {
+          let my = Math.floor((startPoint.y + endPoint.y) / 2);
+          this.currentLine!.points = [
+            startPoint,
+            new Point(startPoint.x, my),
+            new Point(endPoint.x, my),
+            endPoint,
+          ];
+        }
+      } else if (arrowType == "straight" || arrowType == "curve") {
+        this.currentLine?.points.pop();
+        this.currentLine?.points.push(curPoint);
+      }
+    } else if (this.curState == "drawingPath") {
       this.currentLine?.points.pop();
       this.currentLine?.points.push(curPoint);
     }
@@ -55,6 +88,7 @@ export default class ArrowTool {
       Math.floor(e.clientY),
     );
     //
+    let { arrowType } = useToolStyle.getState();
 
     switch (this.curState) {
       case "drawingLine":
@@ -62,18 +96,53 @@ export default class ArrowTool {
           let firstPointInLine = this.currentLine?.points[0];
 
           if (Point.isSamePoint(curPoint, firstPointInLine!)) {
-            this.curState = "drawingPath";
+            if (arrowType == "straight" || arrowType == "curve")
+              this.curState = "drawingPath";
+            else if (arrowType == "snake") {
+              //
+              this.curState = "idle";
+              this.shapeManager.removeShape(this.currentLine!);
+              this.currentLine = null;
+              this.lastPointInLine.x = -1e18;
+              this.lastPointInLine.y = -1e18;
+            }
           } else {
-            let sp = this.currentLine!.points[0];
-            let ep = this.currentLine!.points[1];
-            this.currentLine!.points = [
-              sp,
-              new Point(
-                Math.floor((sp.x + ep.x) / 2),
-                Math.floor((sp.y + ep.y) / 2),
-              ),
-              ep,
-            ];
+            let startPoint = this.currentLine!.points[0];
+            let endPoint =
+              this.currentLine!.points[this.currentLine!.points.length - 1];
+
+            if (arrowType == "straight" || arrowType == "curve") {
+              this.currentLine!.points = [
+                startPoint,
+                new Point(
+                  Math.floor((startPoint.x + endPoint.x) / 2),
+                  Math.floor((startPoint.y + endPoint.y) / 2),
+                ),
+                endPoint,
+              ];
+            } else if (arrowType == "snake") {
+              //          //
+              let dx = Math.abs(endPoint.x - startPoint.x);
+              let dy = Math.abs(endPoint.y - startPoint.y);
+
+              if (dx > dy) {
+                let mx = Math.floor((startPoint.x + endPoint.x) / 2);
+                this.currentLine!.points = [
+                  startPoint,
+                  new Point(mx, startPoint.y),
+                  new Point(mx, endPoint.y),
+                  endPoint,
+                ];
+              } else {
+                let my = Math.floor((startPoint.y + endPoint.y) / 2);
+                this.currentLine!.points = [
+                  startPoint,
+                  new Point(startPoint.x, my),
+                  new Point(endPoint.x, my),
+                  endPoint,
+                ];
+              }
+            }
 
             this.curState = "idle";
           }

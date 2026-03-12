1e8d9743e12bd80082b26170e73686d6557e16e2
Vishwajeet Singh Parihar <svishwajeet750@gmail.com>
2026-03-12 13:12:39 +0000

    grab tool

--- src/classes/Tools/GrabTool.ts
+++ src/classes/Tools/GrabTool.ts
@@ -2,13 +2,20 @@ import type ShapeManager from "../Managers/ShapeManager";
 import type { EventType } from "../Managers/ToolManager";
 import type Tool from "./Tool";
 import type { Tool as ToolType } from "../../store/Tools.store";
+import { Point } from "../Shapes/Point";
 
-type state = "idle" | "drawing";
+type state = "idle" | "moving";
 export default class GrabTool implements Tool {
   toolType: ToolType = "grab";
 
   shapeManager: ShapeManager;
   curState: state = "idle";
+
+  totalMovementX = 0;
+  totalMovementY = 0;
+
+  lastMouseMove = new Point(0, 0);
+  isScreenEmpty = false;
 
   emit: (tool: ToolType, event: EventType) => void;
   constructor(
@@ -19,17 +26,68 @@ export default class GrabTool implements Tool {
     this.emit = emit;
   }
   destructor(): void {}
-  onCanvasMouseDown(e: MouseEvent) {}
 
-  onCanvasMouseMove(e: MouseEvent) {
-    if (this.curState == "drawing") {
+  updateScreenEmpty() {
+    if (
+      this.shapeManager.shapes.some((shape) =>
+        shape.liesInside(
+          new Point(-this.totalMovementX, -this.totalMovementY),
+          new Point(
+            -this.totalMovementX + document.body.clientWidth,
+            -this.totalMovementY + document.body.clientHeight,
+          ),
+        ),
+      )
+    ) {
+      this.isScreenEmpty = false;
+    } else this.isScreenEmpty = true;
+  }
+
+  onCanvasMouseDown(e: MouseEvent) {
+    if (this.curState == "idle") {
+      this.curState = "moving";
+
+      this.lastMouseMove.x = e.clientX;
+      this.lastMouseMove.y = e.clientY;
+
+      this.updateScreenEmpty();
     }
   }
-  onCanvasMouseUp(e: MouseEvent) {}
 
-  onOtherMouseDown(e: MouseEvent): void {}
-  onOtherMouseMove(e: MouseEvent): void {}
-  onOtherMouseUp(e: MouseEvent): void {}
+  onCanvasMouseMove(e: MouseEvent) {
+    if (this.curState == "moving") {
+      document.body.style.cursor = "grabbing";
+
+      let dx = e.clientX - this.lastMouseMove.x;
+      let dy = e.clientY - this.lastMouseMove.y;
+      this.shapeManager.shapes.forEach((shape) =>
+        shape.moveEnclosingRectangle(dx, dy),
+      );
+
+      this.lastMouseMove.x = e.clientX;
+      this.lastMouseMove.y = e.clientY;
+
+      this.totalMovementX += dx;
+      this.totalMovementY += dy;
+
+      this.updateScreenEmpty();
+    } else document.body.style.cursor = "grab";
+  }
+  onCanvasMouseUp(e: MouseEvent) {
+    if (this.curState == "moving") {
+      this.curState = "idle";
+    }
+  }
+
+  onOtherMouseDown(e: MouseEvent): void {
+    document.body.style.cursor = "default";
+  }
+  onOtherMouseMove(e: MouseEvent): void {
+    document.body.style.cursor = "default";
+  }
+  onOtherMouseUp(e: MouseEvent): void {
+    document.body.style.cursor = "default";
+  }
 
   onSwitchTool(oldTool: ToolType, newTool: ToolType): void {}
 }
